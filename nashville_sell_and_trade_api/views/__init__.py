775acc3b79ffae46ae96caba0554c1d2ca1982f6
Brandon De Leon <brandondeleon.615@gmail.com>
2021-12-09 13:33:39 -0600

    modified product view

--- nashville_sell_and_trade_api/views/__init__.py
+++ nashville_sell_and_trade_api/views/__init__.py
@@ -1,1 +1,3 @@
 from .auth import login_user, register_user
+from .product import ProductView
+from .category import CategoryView
---
+++ nashville_sell_and_trade_api/views/product.py
@@ -0,0 +1,95 @@
+from django.core.exceptions import ValidationError
+from rest_framework import status, views
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+
+from nashville_sell_and_trade_api.models import Product, NashUser, Category, category, nash_user, product
+
+
+class ProductView(ViewSet):
+    def create(self, request):
+
+        nash_user = NashUser.objects.get(user=request.auth.user)
+
+        try:
+            product = Product.objects.create(
+                title=request.data['title'],
+                description=request.data["description"],
+                trade=request.data["trade"],
+                price=request.data["price"],
+                image=request.data["image"],
+                quantity=request.data["quantity"],
+                nash_user=nash_user
+
+            )
+            product.categories.set(request.data['categories'])
+            serializer = ProductSerializer(
+                product, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        products = Product.objects.all()
+        categories = request.GET.getlist('category')
+        if len(categories)  != 0:
+            for category in categories:
+                products = products.filter(categories__id=category).distinct() 
+        serializer = ProductSerializer(
+            products, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(
+                product, context={'request': request})
+            return Response(serializer.data)
+        except Product.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+    def update(self, request, pk=None):
+        nash_user= NashUser.objects.get(user=request.auth.user)
+        
+        product = Product.objects.get(pk=pk)
+        product.title=request.data['title']
+        product.description=request.data["description"]
+        product.trade=request.data["trade"]
+        product.price=request.data["price"]
+        product.image=request.data["image"]
+        product.quantity=request.data["quantity"]
+        product.nash_user=nash_user
+        product.categories.set(request.data['categories'])
+        product.save()
+        
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        
+        
+    def destroy(self, request, pk=None):
+
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Product.DoesNotExist as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('id', 'title', 'description', 'categories', 'price',
+                  'image', 'trade', 'quantity', 'nash_user', 'date_posted')
+        depth = 1
