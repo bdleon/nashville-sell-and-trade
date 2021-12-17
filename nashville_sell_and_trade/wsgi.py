d0ceba62e09f54eb981222c9bd0dcb38fb22ffee
Brandon De Leon <brandondeleon.615@gmail.com>
2021-12-17 15:27:37 -0600

    fixed deploy error

--- nashville_sell_and_trade/wsgi.py
+++ nashville_sell_and_trade/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nashvilleSellAndTrade.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nashville_sell_and_trade.settings')
 
 application = get_wsgi_application()
