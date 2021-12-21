9bf186ea9ad0ed3619111d571566de16bfc04bec
Brandon De Leon <brandondeleon.615@gmail.com>
2021-12-20 18:27:35 -0600

    increased max length on modal

--- nashville_sell_and_trade_api/models/nash_user.py
+++ nashville_sell_and_trade_api/models/nash_user.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 class NashUser(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=150)
+    bio = models.CharField(max_length=300)
     picture = models.CharField(max_length=300)
